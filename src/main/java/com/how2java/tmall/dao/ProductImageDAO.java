f5cc9d1721390b4af353bb2fbd51e49db5ec9441
juzized <1658741329@qq.com>
2020-10-28 15:31:59 +0800

    2020.10.28 15:31

--- src/main/java/com/how2java/tmall/dao/ProductImageDAO.java
+++ src/main/java/com/how2java/tmall/dao/ProductImageDAO.java
@@ -1,8 +1,11 @@
 package com.how2java.tmall.dao;
 
+import com.how2java.tmall.pojo.Product;
 import com.how2java.tmall.pojo.ProductImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductImageDAO extends JpaRepository<ProductImage,Integer> {
+import java.util.List;
 
+public interface ProductImageDAO extends JpaRepository<ProductImage,Integer> {
+    public List<ProductImage> findByProductAndTypeOrderByIdDesc(Product product, String type);
 }
